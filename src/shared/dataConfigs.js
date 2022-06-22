9bc92c01dc45cda81e7d0ef794353c4e07989798
Mukund Raj <mraj@broadinstitute.org>
2022-06-22 09:05:19 -0400

    Adding more genes

--- src/shared/dataConfigs.js
+++ src/shared/dataConfigs.js
@@ -4,7 +4,7 @@ export const DATACONFIGS =
       id: 0,
       basePath: "https://storage.googleapis.com/ml_portal/test_data/gene_jsons",
       prefix:"gene_",
-      geneOptions: ['Pcp4', 'Calb1', 'Gng13', 'Gabra6', 'Mbp', 'Plp1', 'Mag', 'Myoc', 'Agt', 'Gfap', 'Slc1a3', 'Aqp4', 'Dcn', 'Flt1', 'Rarres2', 'Foxj1'],
+      geneOptions: ['Pcp4', 'Calb1', 'Gng13', 'Gabra6', 'Mbp', 'Plp1', 'Mag', 'Myoc', 'Agt', 'Gfap', 'Slc1a3', 'Aqp4', 'Dcn', 'Flt1', 'Rarres2', 'Foxj1', 'Xpo7', 'Cul1', 'Herc1', 'Rb1cc1', 'Setd1a', 'Trio', 'Cacna1g', 'Sp4', 'Gria3', 'Grin2a'],
       maxCountMetadataKey: "maxCount",
       title: "Gene Expression",
       relativePath: "test_data2/gene_jsons"
