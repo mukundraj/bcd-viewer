520c39c1b598c4edceedc20663d885e6d4113605
Mukund Raj <mraj@broadinstitute.org>
2022-08-25 13:55:18 -0400

    Populating tableData array of objects for passing to upcoming table component

--- src/components/SingleCellComponent.js
+++ src/components/SingleCellComponent.js
@@ -7,6 +7,7 @@ import {Col, Row, FormGroup, FormLabel} from 'react-bootstrap'
 import RangeSlider from 'react-bootstrap-range-slider';
 import { useState } from 'react';
 import { Typeahead } from 'react-bootstrap-typeahead'; // ES2015
+import produce from "immer";
 
 
 function SingleCell(props){
@@ -27,8 +28,9 @@ function SingleCell(props){
 
   const [maxCellTypes, setMaxCellTypes] = useState(10);
   const [multiSelections, setMultiSelections] = useState([]);
-  const [treeData, setTreeData] = useState([]);
+  const [tableData, setTableData] = useState([]);
   const [geneOptions, setGeneOptions] = useState([]);
+  const [cellTypes, setCellTypes] = useState([]);
   const prevMultiSelections = useRef([]);
 
 
@@ -39,54 +41,60 @@ function SingleCell(props){
     const fetchData = async () => {
       let zloader = new ZarrLoader({zarrPathInBucket});
       let dataGenes = await zloader.getFlatArrDecompressed("z1.zarr/var/human_name/categories");
+      let dataCellTypesRaw = await zloader.getFlatArrDecompressed("z1.zarr/obs/_index");
       // let dataX = await zloader.getDataColumn("z1.zarr/X", 0);
+
+      let myRe = /=([\s\S]*)$/
+      let dataCellTypes = dataCellTypesRaw.map(x=>myRe.exec(x)[0].slice(1));
       console.log(dataGenes);
+      console.log(dataCellTypes);
       setGeneOptions(dataGenes);
+      setCellTypes(dataCellTypes);
+      let initTableData = new Array(dataCellTypes.length).fill({})
+      initTableData = initTableData.map((x,i)=>{return {"ct":dataCellTypes[i]}});
+      setTableData(initTableData);
     }
-    
+
     fetchData();
 
   }, []);
 
 
-
+  // updating tableData json array on change in selected genes
   useEffect(()=>{
     const fetchData = async (col_idx) => {
       let zloader = new ZarrLoader({zarrPathInBucket});
       let dataCol = await zloader.getDataColumn("z1.zarr/X", col_idx);
-      console.log(dataCol);
+      let tableDataTmp = tableData.map((x,i)=>produce(x, draft=>{
+        draft[col_idx] = dataCol[i];
+      }));
+      setTableData(tableDataTmp);
     }
 
-    // identify newly added or removed gene
+    // identify newly added or removed gene and update tableData accordingly
     let added = null, removed = null;
     if (prevMultiSelections.current.length<multiSelections.length){
       added = multiSelections.filter(x => !prevMultiSelections.current.includes(x));
       console.log('added', added);
       let colIdx = geneOptions.indexOf(added[0]);
-      fetchData(colIdx);
-
+      fetchData(colIdx); // reminder: async function
     }else if(prevMultiSelections.current.length>multiSelections.length){
-
       removed = prevMultiSelections.current.filter(x => !multiSelections.includes(x));
       console.log('removed', removed);
-    }
-
-    if (added){
-    // read newly selected column and add to json array
-    
-
-    }else if (removed){
-    // remove dropped column if any from json array
-    
+      let colIdx = geneOptions.indexOf(removed[0]);
+      let tableDataTmp = tableData.map((x,i)=>produce(x, draft=>{
+        delete draft[colIdx];
+      }));
+      setTableData(tableDataTmp);
 
     }
 
-    
     console.log('multiSelections', multiSelections);
     prevMultiSelections.current=multiSelections;
 
   },[multiSelections]);
-  
+
+
   return(
     <>
       <Breadcrumbs/>
@@ -105,13 +113,13 @@ function SingleCell(props){
         </Col>
         <Col xs="2">Max #celltypes:</Col>
         <Col xs="2">
-            <RangeSlider
-              value={maxCellTypes}
-              onChange={e => setMaxCellTypes(e.target.value)}
-              min={0}
-              max={100}
-              step={1}
-            />
+          <RangeSlider
+            value={maxCellTypes}
+            onChange={e => setMaxCellTypes(e.target.value)}
+            min={0}
+            max={100}
+            step={1}
+          />
         </Col>
       </Row>
     </>
