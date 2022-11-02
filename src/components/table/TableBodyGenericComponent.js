ec6dcaa3c265b66bfda5af79f6b772a4d66fde6b
Mukund Raj <mraj@broadinstitute.org>
2022-11-02 14:53:50 -0400

    Add tooltip shoing pid to RegionExp buttons

--- src/components/table/TableBodyGenericComponent.js
+++ src/components/table/TableBodyGenericComponent.js
@@ -24,7 +24,7 @@ const TableBodyGeneric = ({ columns, tableDataSorted, setDataLoadStatus}) => {
             <tr key={data.key}>
               {columns.map(({ accessor }) => {
                 if (accessor==='g')
-                  return <td key={accessor}><button onClick={()=>updateChosenGene(data[accessor], data['p'])}>{data[accessor]}</button></td>;
+                  return <td key={accessor}><button className="regexptooltip" onClick={()=>updateChosenGene(data[accessor], data['p'])}>{data[accessor]}<span className='regexptooltiptext'>pid:{pidToSrno[data['p']]}</span></button></td>;
                 else
                   return <td key={accessor}>{data[accessor]}</td>;
               })}
