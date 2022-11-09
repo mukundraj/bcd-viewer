c9c2851f43fd436c7163c1edc39f95f9f931270f
Mukund Raj <mraj@broadinstitute.org>
2022-11-09 10:18:29 -0500

    Add error handling for case of missing celltype for SingleCell component

--- src/components/table/TableBodyComponent.js
+++ src/components/table/TableBodyComponent.js
@@ -28,23 +28,27 @@ const TableBody = ({columns, tableDataSorted}) => {
       // navigate('/cellspatial', {state:{celltype:celltype}});
 
       // get freqBar data for this celltype to derminne maxima puckid
-      const fetchData = async (celltype) => {
-        let fbarsDataUrl = `https://storage.googleapis.com/bcdportaldata/cellspatial_data/freqbars/cell_jsons_s2c/${celltype}.json`
-        const readData = await fetch(fbarsDataUrl)
-         .then(response => response.json())
-          .catch(error => {
-            alert("Could not find spatial data for this cell type");
-            console.log(error);
-          });
-      
-        // find maxima pid
-        const counts = readData.sorted_puckwise_cnts.map(o => o.cnt);
-        const maxIdx = counts.indexOf(Math.max(...counts));
-        const maximaPid = readData.sorted_puckwise_cnts[maxIdx].key[0];
+    const fetchData = async (celltype) => {
+      let fbarsDataUrl = `https://storage.googleapis.com/bcdportaldata/cellspatial_data/freqbars/cell_jsons_s2c/${celltype}.json`
+      const readData = await fetch(fbarsDataUrl)
+        .then(response => response.json())
+        .then(readData => {
+          // find maxima pid
+          const counts = readData.sorted_puckwise_cnts.map(o => o.cnt);
+          const maxIdx = counts.indexOf(Math.max(...counts));
+          const maximaPid = readData.sorted_puckwise_cnts[maxIdx].key[0];
 
-        setChosenPuckid({...chosenPuckid, pid:maximaPid, cell:celltype});
-        navigate('/cellspatial');
-      }
+          setChosenPuckid({...chosenPuckid, pid:maximaPid, cell:celltype});
+          navigate('/cellspatial');
+          return readData;
+        } )
+        .catch(error => {
+          alert("Could not find spatial data for this cell type");
+          console.log(error);
+          return undefined;
+        });
+
+    }
 
       // get celltype freqbar data and set maxima pid
       fetchData(celltype);
@@ -65,7 +69,7 @@ const TableBody = ({columns, tableDataSorted}) => {
                 const rFactor = isNaN(tData)?0:tData;
                 return <td key={accessor}>
                   {isNaN(tData)?
-                    <><button className="btn btn-light btn-sm py-0" style={{borderWidth:"0"}} onClick={()=>{toCellSpatial(data[accessor])}}>{data[accessor]}</button>, <span style={{color:'#8E44AD'}}>{data.cc}</span>, <span style={{color:'#BB8FCE'}}>{data.pct}%</span></>:tData===0?"-":""}
+                    <><a className="btn btn-light btn-sm py-0" style={{borderWidth:"0"}} onClick={()=>{toCellSpatial(data[accessor])}}>{data[accessor]}</a>, <span style={{color:'#8E44AD'}}>{data.cc}</span>, <span style={{color:'#BB8FCE'}}>{data.pct}%</span></>:tData===0?"-":""}
                   <span style={{width:rFactor*radius, height:rFactor*radius, backgroundColor:computedColor(data[-accessor])}} className="dot sctooltip">
                     <span className="sctooltiptext">{Math.round(data[-accessor]*100)/100}, {Math.round(data[accessor]*100)}%</span>
                   </span>
