350918f724a4373aa45d4e28234cec5049520098
Mukund Raj <mraj@broadinstitute.org>
2023-05-04 09:35:38 -0400

    Update SC table e2e tests for new table format

--- cypress/e2e/all/singlecell.cy.js
+++ cypress/e2e/all/singlecell.cy.js
@@ -18,35 +18,35 @@ cy.get('.rbt-input-main')
       .type('{downarrow}')
       .type('{enter}')
 
-    cy.get('.sctable')
-      .contains('td', 'Ex_Cyp26b1_Slc30a3_Ptprc_3')
+    cy.get('.table')
+      .contains('td', 'Ex-Cyp26b1-Slc30a3-Ptprc-3')
 
-    cy.get('.sctable')
-      .contains('td', 'Ex_Rorb_Fibcd1_Syt6')
+    cy.get('.table')
+      .contains('td', 'Ex-Rorb-Fibcd1-Syt6')
 
-    cy.get('.sctable')
-      .contains('td', 'Ex_Iqcj_Ms4a15_1')
+    cy.get('.table')
+      .contains('td', 'Ex-Iqcj-Ms4a15-1')
 
-    cy.get('.sctable')
-      .contains('td', 'Ex_Iqcj_Ms4a15_2')
+    cy.get('.table')
+      .contains('td', 'Ex-Iqcj-Ms4a15-2')
 
-    cy.get('.sctable')
-      .contains('td', 'Ex_Slc30a3_Scn5a')
+    cy.get('.table')
+      .contains('td', 'Ex-Slc30a3-Scn5a')
 
-    cy.get('.sctable')
-      .contains('td', 'Ex_Ccn3_Fezf2_Mndal')
+    cy.get('.table')
+      .contains('td', 'Ex-Ccn3-Fezf2-Mndal')
 
-    cy.get('.sctable')
-      .contains('td', 'Ex_Crym_Csf2rb2')
+    cy.get('.table')
+      .contains('td', 'Ex-Crym-Csf2rb2')
 
-    cy.get('.sctable')
-      .contains('td', 'Ex_Crym_Cldn22')
+    cy.get('.table')
+      .contains('td', 'Ex-Crym-Cldn22')
 
-    cy.get('.sctable')
-      .contains('td', 'Ex_Trabd2b_Slc30a3_Vgll3')
+    cy.get('.table')
+      .contains('td', 'Ex-Trabd2b-Slc30a3-Vgll3')
 
-    cy.get('.sctable')
-      .contains('td', 'Ex_Rorb_Scn5a_Rxrg')
+    cy.get('.table')
+      .contains('td', 'Ex-Rorb-Scn5a-Rxrg')
 
     cy.wait(1000)
     cy.get('#maxCellTypesSlider')
@@ -55,7 +55,7 @@ cy.get('.rbt-input-main')
       
     cy.wait(1000)
 
-    cy.get('.sctable')
+    cy.get('.table')
       .contains('td', 'Ex_Rorb_Scn5a_Rxrg').should('not.exist')
 
  }) 
@@ -68,23 +68,23 @@ cy.get('.rbt-input-main')
       .type('{downarrow}')
       .type('{enter}')
     
-    cy.get('.sctable')
-      .contains('td', 'DopEx_Calb1_Col6a5')
+    cy.get('.table')
+      .contains('td', 'DopEx-Calb1-Col6a5')
 
-    cy.get('.sctable')
-      .contains('td', 'Inh_Vip_Sncg_Yjefn3')
+    cy.get('.table')
+      .contains('td', 'Inh-Vip-Sncg-Yjefn3')
 
-    cy.get('.sctable')
-      .contains('td', 'DopEx_Calb1_Npw_1')
+    cy.get('.table')
+      .contains('td', 'DopEx-Calb1-Npw-1')
 
-    cy.get('.sctable')
-      .contains('td', 'Inh_Vip_Crh_Sema5b')
+    cy.get('.table')
+      .contains('td', 'Inh-Vip-Crh-Sema5b')
 
-    cy.get('.sctable')
-      .contains('td', 'Inh_Vip_Sncg_Slit1')
+    cy.get('.table')
+      .contains('td', 'Inh-Vip-Sncg-Slit1')
 
-    cy.get('.sctable')
-      .contains('td', 'Inh_Vip_Grpr')
+    cy.get('.table')
+      .contains('td', 'Inh-Vip-Grpr')
 
       cy.wait(1000)
     cy.get('#maxCellTypesSlider')
@@ -92,25 +92,25 @@ cy.get('.rbt-input-main')
       .trigger('change')
 
       cy.wait(1000)
-    cy.get('.sctable')
-      .contains('td', 'Inh_Vip_Grpr').should('not.exist')
+    cy.get('.table')
+      .contains('td', 'Inh-Vip-Grpr').should('not.exist')
 
     cy.get('#rdts1-1').check() // selecting fiber tracts
 
-    cy.get('.sctable')
-      .contains('td', 'Ex_Pou4f1_Htr5b_7')
+    cy.get('.table')
+      .contains('td', 'Ex-Pou4f1-Htr5b-7')
 
-    cy.get('.sctable')
-      .contains('td', 'Ex_Nxph4_Moxd1_Igfbp4')
+    cy.get('.table')
+      .contains('td', 'Ex-Nxph4-Moxd1-Igfbp4')
 
-    cy.get('.sctable')
-      .contains('td', 'Inh_Lhx6_Sst_Ccna1_3')
+    cy.get('.table')
+      .contains('td', 'Inh-Lhx6-Sst-Ccna1-3')
 
-    cy.get('.sctable')
-      .contains('td', 'Ex_Nxph4_Ror1')
+    cy.get('.table')
+      .contains('td', 'Ex-Nxph4-Ror1')
     
-    cy.get('.sctable')
-      .contains('td', 'Ex_Nxph4_Kynu')
+    cy.get('.table')
+      .contains('td', 'Ex-Nxph4-Kynu')
 
   })
 })
